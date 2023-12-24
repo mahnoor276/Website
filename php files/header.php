3d496ad2ff8d5d216600a2bbc4ef82cfef21c0c7
mahnoor276 <148251643+mahnoor276@users.noreply.github.com>
2023-12-24 22:14:47 +0500

    Add files via upload

---
+++ php files/header.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 px-4 px-lg-5">
+        <a href="index.php" class="navbar-brand d-flex align-items-center">
+            <h2 class="m-0 text-primary">Cyber Solutions</h2>
+        </a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-4 py-lg-0">
+              <!--  <a href="" class="nav-item nav-link active">Home</a> -->
+                <a href="index.html" class="nav-item nav-link">Home</a>
+                <a href="company.html" class="nav-item nav-link">Company</a>
+                <a href="service.html" class="nav-item nav-link">Services</a>
+               <!-- <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu rounded-0 rounded-bottom m-0">
+                        <a href="feature.html" class="dropdown-item">Feature</a>
+                        <a href="quote.html" class="dropdown-item">Free Quote</a>
+                        <a href="team.html" class="dropdown-item">Our Team</a>
+                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                        <a href="404.html" class="dropdown-item">404 Page</a>
+                    </div>
+                </div>-->
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">About Us</a>
+                    <div class="dropdown-menu rounded-0 rounded-bottom m-0">
+                        <a href="target market.html" class="dropdown-item">Target Market </a>
+                        <a href="marketing mix.html" class="dropdown-item">Marketing Mix</a>
+                        <a href="reflections.html" class="dropdown-item">Reflections</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <div class="h-100 d-lg-inline-flex align-items-center d-none">
+                <a class="btn btn-square rounded-circle bg-light text-primary me-2" href=""><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-square rounded-circle bg-light text-primary me-2" href=""><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-square rounded-circle bg-light text-primary me-2" href=""><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-square rounded-circle bg-light text-primary me-0" href=""><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </nav>
